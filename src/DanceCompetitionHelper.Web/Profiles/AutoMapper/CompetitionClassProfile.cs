bc3c36bf4052ca9c5c5e692a5b83903ff456d471
Michael Tscherny <michael.tscherny@gmx.at>
2024-07-19 09:12:24 +0200

    #20 * add missing CompetitionClassProfile

---
+++ src/DanceCompetitionHelper.Web/Profiles/AutoMapper/CompetitionClassProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DanceCompetitionHelper.Web.Models.CompetitionClassModels;
+using Tables = DanceCompetitionHelper.Database.Tables;
+
+namespace DanceCompetitionHelper.Web.Profiles.AutoMapper
+{
+    public class CompetitionClassProfile : Profile
+    {
+        public CompetitionClassProfile()
+        {
+            CreateMap<CompetitionClassViewModel, Tables.CompetitionClass>();
+            CreateMap<Tables.CompetitionClass, CompetitionClassViewModel>();
+        }
+    }
+}
