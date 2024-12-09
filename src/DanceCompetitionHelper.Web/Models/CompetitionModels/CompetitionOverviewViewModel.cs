1a9bc3f8525d29d7ea362de7b0618e683d6772f3
Michael Tscherny <michael.tscherny@gmx.at>
2024-12-09 16:48:54 +0100

    #20 * add missed CompetitionOverviewViewModel.cs

---
+++ src/DanceCompetitionHelper.Web/Models/CompetitionModels/CompetitionOverviewViewModel.cs
@@ -0,0 +1,8 @@
+﻿using DanceCompetitionHelper.Database.Tables;
+
+namespace DanceCompetitionHelper.Web.Models.CompetitionModels
+{
+    public class CompetitionOverviewViewModel : OverviewModelBase<Competition>
+    {
+    }
+}
